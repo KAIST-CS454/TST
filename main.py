778084a8fc0f31e7d253c5c19ad6c4547a16de37
Yihan Kim <kabi@kaist.ac.kr>
2017-12-14 20:36:49 +0900

    * -> for i in ..

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from ACOUnit import ACORun
 
 def main(args):
 
-	tstSeq, seqFit = ACORun(antSize = 20, generation = 200, seqLength = 30, initP = 10, rho = 0.7, alpha = 0.5, EFGfile = args[0])
+	tstSeq, seqFit = ACORun(antSize = 200, generation = 40, seqLength = 100, initP = 200, rho = 0.7, alpha = 0.5, EFGfile = args[0])
 	# generation number = 100
 	# ant population size = 20
 	# seqence length = 20
